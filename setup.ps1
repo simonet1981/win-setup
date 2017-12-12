07902b4425c2d11736155bfaa5fef1fd3c2250b3
simonet1981 <simonet1981@gmail.com>
2017-12-12 14:43:21 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -9,6 +9,9 @@ Update-ExecutionPolicy RemoteSigned
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions -EnableShowFullPathInTitleBar
 #Enable-RemoteDesktop
 
+#install Chocolatey
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
 # Dev Tools
 cinst conemu
 #cinst powershell
